92b0f4a82edecaa8b96e61dd09ec7b3f34a70d41
mimichu <chuer.pan@gmail.com>
2025-11-03 21:46:39 -0800

    clean up bash script to run fs on zed 2i stereo imgs

--- foundation_stereo_run_zed.sh
+++ foundation_stereo_run_zed.sh
@@ -1,22 +1,17 @@
+# Example from foundation stereo demo
 # python scripts/run_demo.py \
 #     --left_file ./assets/left.png \
 #     --right_file ./assets/right.png \
 #     --ckpt_dir ./pretrained_models/23-51-11/model_best_bp2.pth \
 #     --out_dir ./test_outputs/ 
 
-# python scripts/run_demo.py \
-#     --left_file ~/Documents/ZED/processed/HD2K_SN39914083_18-44-59_left/frame_000000.png \
-#     --right_file ~/Documents/ZED/processed/HD2K_SN39914083_18-44-59_right/frame_000000.png \
-#     --intr_file /home/chuerpan/repo/InteractionRetarget/zed_intrinsics.txt \
-#     --ckpt_dir ./pretrained_models/23-51-11/model_best_bp2.pth \
-#     --out_dir ./test_outputs/ZED/HD2K_SN39914083_18-44-59_000000/ \
-#     --hiera 1
+svo_name="HD2K_SN39914083_18-44-59"
+frame_idx="000000"
 
- 
 python scripts/run_demo.py \
-    --left_file ~/Documents/ZED/processed/HD2K_SN39914083_18-44-59_left/frame_000000.png \
-    --right_file ~/Documents/ZED/processed/HD2K_SN39914083_18-44-59_right/frame_000000.png \
+    --left_file ~/Documents/ZED/processed_2/${svo_name}_left/frame_${frame_idx}.png \
+    --right_file ~/Documents/ZED/processed_2/${svo_name}_right/frame_${frame_idx}.png \
     --intrinsic_file /home/chuerpan/repo/InteractionRetarget/cam_configs/zed_2i/zed_intrinsics_foundation_stereo.txt \
     --ckpt_dir ./pretrained_models/23-51-11/model_best_bp2.pth \
-    --out_dir ./test_outputs/ZED/HD2K_SN39914083_18-44-59_000000/ \
+    --out_dir ./test_outputs/ZED/${svo_name}_${frame_idx}/ \
     --hiera 1
